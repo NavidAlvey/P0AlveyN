a0492cb42ee5248fb550fdc2b65783aa6891e496
Navid Alvey <navidalvey@gmail.com>
2025-11-19 11:37:12 -0600

    Updated User.java file with getters/setters to create a user within database

--- src/main/java/com/revature/P0AlveyN/entity/User.java
+++ src/main/java/com/revature/P0AlveyN/entity/User.java
@@ -1,5 +1,70 @@
-package main.java.com.revature.P0AlveyN.entity;
+package com.revature.P0AlveyN.entity;
 
 public class User {
+
+    // Fields
+    private Long id;
+    private String name;
+    private String lastFourDigits;
+    private boolean primaryCardholder;
+
+    // Constructor
+    public User() {
+       
+    }
+
+    public User(String name, String lastFourDigits, boolean primaryCardholder) {
+        setName(name);
+        setLastFourDigits(lastFourDigits);
+        this.primaryCardholder = primaryCardholder;
+    }
+
+    // Methods
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastFourDigits() {
+        return lastFourDigits;
+    }
+
+    public boolean isPrimaryCardholder() {
+        return primaryCardholder;
+    }
     
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        
+        this.name = name.trim();
+    }
+
+    public void setLastFourDigits(String lastFourDigits) {
+        
+        this.lastFourDigits = lastFourDigits;
+    }
+
+    public void setPrimaryCardholder(boolean primaryCardholder) {
+        this.primaryCardholder = primaryCardholder;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastFourDigits='" + lastFourDigits + '\'' +
+                ", primaryCardholder=" + primaryCardholder +
+                '}';
+    }
 }
