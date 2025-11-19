bef16488ccee71fd7b33ed140943912b33f9de80
Navid Alvey <navidalvey@gmail.com>
2025-11-18 21:37:40 -0600

    Initial project commit

---
+++ src/main/java/com/revature/P0AlveyN/P0AlveyNApplication.java
@@ -0,0 +1,13 @@
+package com.revature.P0AlveyN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P0AlveyNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P0AlveyNApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/P0AlveyN/P0AlveyNApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.P0AlveyN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P0AlveyNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
