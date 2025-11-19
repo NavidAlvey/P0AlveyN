01b197937f6f93bbcdd7f22e8ff73d8c41e709a4
Navid Alvey <navidalvey@gmail.com>
2025-11-18 22:25:36 -0600

    Cleaned up some SpringBoot errors and outline project structure

--- src/main/java/com/revature/P0AlveyN/Main.java
+++ src/main/java/com/revature/P0AlveyN/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class P0AlveyNApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(P0AlveyNApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
---
+++ src/main/java/com/revature/P0AlveyN/repository/TransactionRepository.java
@@ -0,0 +1,5 @@
+package main.java.com.revature.P0AlveyN.repository;
+
+public class TransactionRepository {
+    
+}
