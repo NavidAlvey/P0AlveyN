baf0d999738a2f5c9a27967f6e5995c1e2293e40
Navid Alvey <navidalvey@gmail.com>
2025-11-19 21:47:27 -0600

    Added fields and constructor to Transaction class. May need to add additional fields for further parameters when working with each table in databse

--- src/main/java/com/revature/P0AlveyN/entity/Transaction.java
+++ src/main/java/com/revature/P0AlveyN/entity/Transaction.java
@@ -1,5 +1,33 @@
-package main.java.com.revature.P0AlveyN.entity;
+package com.revature.P0AlveyN.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 
 public class Transaction {
-    
+
+    //Fields
+    private Long id;
+    private LocalDate transactionDate;
+    private String vendor;
+    private BigDecimal amount;
+    private String cardLastFour;
+    private int type;
+    private String description;
+
+    // Constructors
+    public Transaction() {}
+
+    public Transaction(LocalDate transactionDate, String vendor, BigDecimal amount, String cardLastFour, int type, String description) {
+        this.transactionDate = transactionDate;
+        this.vendor = vendor;
+        this.amount = amount;
+        this.cardLastFour = cardLastFour;
+        this.type = type;
+        this.description = description;
+    }
+
+    // Methods
+
+
 }
