4a583249daa438cfbbe0dbe390ed3e10e9f50a99
Navid Alvey <navidalvey@gmail.com>
2025-11-20 20:41:56 -0600

    Added functionality to save a user to the database

--- src/main/java/com/revature/P0AlveyN/repository/UserRepository.java
+++ src/main/java/com/revature/P0AlveyN/repository/UserRepository.java
@@ -1,5 +1,46 @@
-package main.java.com.revature.P0AlveyN.repository;
+package com.revature.P0AlveyN.repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.revature.P0AlveyN.entity.User;
+import com.revature.P0AlveyN.util.DatabaseConnection;
 
 public class UserRepository {
+    
+    //Saves a user to the database using SQL commands
+    public User save(User user) throws SQLException {
+        String sql = "INSERT INTO users (name, last_four, primary_cardholder) VALUES (?, ?, ?)";
+        
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            
+            pstmt.setString(1, user.getName());
+            pstmt.setString(2, user.getLastFourDigits());
+            pstmt.setBoolean(3, user.isPrimaryCardholder());
+            
+            int affectedRows = pstmt.executeUpdate();
+            
+            if (affectedRows == 0) {
+                throw new SQLException("Creating user failed, no rows affected.");
+            }
+            
+            try (ResultSet generatedKeys = pstmt.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    user.setId(generatedKeys.getLong(1));
+                } else {
+                    throw new SQLException("Creating user failed, no ID obtained.");
+                }
+            }
+        }
+        
+        return user;
+    }
     
 }
