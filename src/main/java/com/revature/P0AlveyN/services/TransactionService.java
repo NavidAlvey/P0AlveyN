7dcac3eed110dbf0770910f7ab7d76297e43baf8
Navid Alvey <navidalvey@gmail.com>
2025-11-20 22:51:57 -0600

    Initialized all the fields and this giant constructor which calls all the other transaction repositories

--- src/main/java/com/revature/P0AlveyN/services/TransactionService.java
+++ src/main/java/com/revature/P0AlveyN/services/TransactionService.java
@@ -1,5 +1,42 @@
-package main.java.com.revature.P0AlveyN.services;
+package com.revature.P0AlveyN.services;
+
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+import com.revature.P0AlveyN.entity.Category;
+import com.revature.P0AlveyN.entity.Transaction;
+import com.revature.P0AlveyN.entity.TransactionSplit;
+import com.revature.P0AlveyN.entity.TransactionType;
+import com.revature.P0AlveyN.entity.User;
+import com.revature.P0AlveyN.repository.TransactionCategoryRepository;
+import com.revature.P0AlveyN.repository.TransactionRepository;
+import com.revature.P0AlveyN.repository.TransactionSplitRepository;
 
 public class TransactionService {
-    
+
+    private final TransactionRepository transactionRepository;
+    private final TransactionSplitRepository splitRepository;
+    private final TransactionCategoryRepository transactionCategoryRepository;
+    private final CategoryService categoryService;
+    private final UserService userService;
+
+    public TransactionService(TransactionRepository transactionRepository,
+                              TransactionSplitRepository splitRepository,
+                              TransactionCategoryRepository transactionCategoryRepository,
+                              CategoryService categoryService,
+                              UserService userService) {
+        this.transactionRepository = transactionRepository;
+        this.splitRepository = splitRepository;
+        this.transactionCategoryRepository = transactionCategoryRepository;
+        this.categoryService = categoryService;
+        this.userService = userService;
+    }
+
 }
